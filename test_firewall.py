213cab623309f895cb653b0e20881af3e9088c64
mrsaicharan1 <saicharan.reddy1@gmail.com>
2018-10-01 21:17:12 +0530

    Uploaded test cases file

--- test_firewall.py
+++ test_firewall.py
@@ -1,8 +1,13 @@
 import unittest
 from firewall import Firewall
 class TestFireWall(unittest.TestCase):
-    def test_ip_address(self,ip):
-        self.assertTrue("192.168.1.5-192.168.255.255")
+
+    def test(self):
+        fw = Firewall('input.csv')
+        self.assertTrue(fw.accept_packet("inbound","udp","80","192.168.1.6"),True)
+        self.assertTrue(fw.accept_packet("outbound","tcp","25","173.177.2.3"),True)
+        self.assertFalse(fw.accept_packet("inbound","udp","9","192.167.1.6"),False)
+        self.assertFalse(fw.accept_packet("inbound","udp","72","192.158.1.6"),False)
 
 if __name__ == "__main__":
     unittest.main()
